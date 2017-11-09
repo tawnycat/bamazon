1174f9385a1a59fed1c4ed7602750cc38fba02cd
tawnycat <tawnyeleanor@gmail.com>
2017-11-08 23:28:42 -0500

    finished seed file

---
+++ seed.sql
@@ -0,0 +1,29 @@
+INSERT INTO products (product_name, department_name, price, stock_quantity)
+VALUES ("milk", "dairy", 3.99, 10);
+
+INSERT INTO products (product_name, department_name, price, stock_quantity)
+VALUES ("bread", "bakery", 2.99, 10);
+
+INSERT INTO products (product_name, department_name, price, stock_quantity)
+VALUES ("eggs", "dairy", 2.99, 10);
+
+INSERT INTO products (product_name, department_name, price, stock_quantity)
+VALUES ("apple", "produce", 0.50, 30);
+
+INSERT INTO products (product_name, department_name, price, stock_quantity)
+VALUES ("banana", "produce", 0.39, 60);
+
+INSERT INTO products (product_name, department_name, price, stock_quantity)
+VALUES ("ice cream", "dairy", 4.99, 5);
+
+INSERT INTO products (product_name, department_name, price, stock_quantity)
+VALUES ("rice", "dry goods", 2.99, 10);
+
+INSERT INTO products (product_name, department_name, price, stock_quantity)
+VALUES ("cheese", "dairy", 2.99, 10);
+
+INSERT INTO products (product_name, department_name, price, stock_quantity)
+VALUES ("juice", "drinks", 3.99, 10);
+
+INSERT INTO products (product_name, department_name, price, stock_quantity)
+VALUES ("bottled water", "drinks", 9.99, 20);
